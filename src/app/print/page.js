b78ea4f0ed0249918e2c1fef6ce2954d94b7e85c
bomboonsan <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>
2025-04-10 14:03:19 +0700

    fix bug 4

--- src/app/print/page.js
+++ src/app/print/page.js
@@ -712,7 +712,19 @@ export default function Record() {
                                             <p className="text-[1rem] lg:text-xl font-medium text-center">
                                             CACS-CL
                                             </p>
-                                            <p className={CACS < 7 ? "result-circle-text text-[#74b8e4]" : CACS >= 7 && CACS < 17 ? "result-circle-text text-[#45bc8d]" : CACS >= 17 ? "result-circle-text text-[#fbef20]" : "" }>{getRiskLevel(CACS)}</p>
+                                            <p className={
+                                                CACS < 7 ? "result-circle-text text-[#74b8e4]" 
+                                                : 
+                                                CACS >= 7 && CACS < 17 ? "result-circle-text text-[#45bc8d]" 
+                                                : 
+                                                CACS >= 17 && CACS < 100 ? "result-circle-text text-[#fbef20]" 
+                                                :
+                                                CACS >= 100 && CACS < 1000 ? "result-circle-text text-[#fbae3d]" 
+                                                :
+                                                CACS >= 1000 ? "result-circle-text text-[#f2654c]" 
+                                                : 
+                                                "" 
+                                                }>{getRiskLevel(CACS)}</p>
                                         </div>
                                         <div>
                                             <div className="flex flex-row gap-1 lg:gap-5 items-center justify-center">
@@ -727,12 +739,24 @@ export default function Record() {
                                                 </div>
                                                 <div className="flex-initial">                                                                           
                                                     <div className="text-center">
-                                                        <span className={CACS < 7 ? "inline-flex aspect-square items-center justify-center text-[1.2rem] lg:text-3xl bg-[#74b8e4] text-white size-14 lg:size-24 rounded-full" : CACS >= 7 && CACS < 17 ? "inline-flex aspect-square items-center justify-center text-[1.2rem] lg:text-3xl bg-[#45bc8d] text-white size-14 lg:size-24 rounded-full" : CACS >= 17 ? "inline-flex aspect-square items-center justify-center text-[1.2rem] lg:text-3xl bg-[#fbef20] text-white size-14 lg:size-24 rounded-full" : "" }>
+                                                        <span className={
+                                                            CACS < 7 ? "inline-flex aspect-square items-center justify-center text-[1.2rem] lg:text-3xl bg-[#74b8e4] text-white size-14 lg:size-24 rounded-full" 
+                                                            : 
+                                                            CACS >= 7 && CACS < 17 ? "inline-flex aspect-square items-center justify-center text-[1.2rem] lg:text-3xl bg-[#45bc8d] text-white size-14 lg:size-24 rounded-full" 
+                                                            : 
+                                                            CACS >= 17 && CACS < 100 ? "inline-flex aspect-square items-center justify-center text-[1.2rem] lg:text-3xl bg-[#fbef20] text-white size-14 lg:size-24 rounded-full" 
+                                                            :
+                                                            CACS >= 100 && CACS < 1000 ? "inline-flex aspect-square items-center justify-center text-[1.2rem] lg:text-3xl bg-[#fbae3d] text-white size-14 lg:size-24 rounded-full" 
+                                                            :
+                                                            CACS >= 1000 ? "inline-flex aspect-square items-center justify-center text-[1.2rem] lg:text-3xl bg-[#f2654c] text-white size-14 lg:size-24 rounded-full" 
+                                                            : 
+                                                            "" 
+                                                            }>
                                                             {
                                                                 // CACS >= 100 ? 100 : Number(CACS).toFixed(0)
                                                                 Number(CACS).toFixed(0)
-                                                            } 
-                                                            <span className='text-base'>%</span>
+                                                            }
+                                                             <span className='text-base'>%</span>
                                                         </span>
                                                     </div>
                                                 </div>
